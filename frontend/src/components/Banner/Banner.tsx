9652c8256523111a42e80a9b6ec98df6dd4c8864
gemrx <gemrdev@gmail.com>
2024-10-31 21:13:15 -0500

    Banner finished

--- frontend/src/components/Banner/Banner.tsx
+++ frontend/src/components/Banner/Banner.tsx
@@ -1,68 +1,90 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Banner.module.css';
 
-export default function Banner() {
-  const announcements = [
-    'Free Shipping On All Orders',
-    'Get 10% Off When Buying 2+ Plants',
-    'Free Personalized Gift Notes',
-  ];
+type Animation = 'slideIn' | 'slideOut';
 
+interface BannerProps {
+  announcements: string[],
+}
+
+interface AnnouncementCarouselProps {
+  announcements: string[]
+  displayInterval?: number
+}
+
+// Main Component
+export default function Banner({ announcements }: BannerProps) {
   return (
-    <div className={styles.AnnouncementBar}>
+    <div className={styles.Banner}>
       <div className={styles.container}>
         <div className={styles.title}>Bring Nature Indoors</div>
-        <AnnouncementTicker announcements={announcements}/>
+        <AnnouncementCarousel announcements={announcements}/>
       </div>
     </div>
   );
 }
 
-interface AnnouncementTickerProps {
-  announcements: string[]
-  displayInterval?: number
-}
+// Subcomponent
+function AnnouncementCarousel({ announcements, displayInterval = 2500 }: AnnouncementCarouselProps) {
+  const [firstAnnouncementIndex, setFirstAnnouncementIndex] = useState(0);
+  const [secondAnnouncementIndex, setSecondAnnouncementIndex] = useState(1);
+  
+  const [firstAnnouncementAnimation, setFirstAnnouncementAnimation] = useState<Animation>('slideIn');
+  const [secondAnnouncementAnimation, setSecondAnnouncementAnimation] = useState<Animation | null>(null);
 
-function AnnouncementTicker({ announcements, displayInterval = 2500 }: AnnouncementTickerProps) {
-  const [index, setIndex] = useState(0);
-  const [animationType, setAnimationType] = useState('slideIn');
-  const timeOutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  function toggleAnimations() {
+    setFirstAnnouncementAnimation(prev => (prev === 'slideIn' ? 'slideOut' : 'slideIn'));
+    setSecondAnnouncementAnimation(prev => (prev === 'slideIn' ? 'slideOut' : 'slideIn'));
+  }
 
+  function handleAnimationEnd(event: React.AnimationEvent<HTMLDivElement>, announcementType: 'first' | 'second') {
+    const animationName = event.animationName; // encrypted by CSS Modules
 
-  // Manage transition between announcements
-  function handleAnimationEnd() {
-    if (animationType === 'slideIn') {
-
-      // wait a few seconds before showing the next announcement
-      timeOutRef.current = setTimeout(() => {
-        setAnimationType('slideOut');
-      }, displayInterval);
-
-    } else {
-
-      // change to the next announcement and reset the animation
-      setIndex((index + 1) % announcements.length);
-      setAnimationType('slideIn');
+    // Only update the announcement's index when the announcement slide out
+    if (animationName.includes('slideOut')) {
+      if (announcementType === 'first') {
+        setFirstAnnouncementIndex(prevIndex => (prevIndex + 2) % announcements.length);
+      } else if (announcementType === 'second') {
+        setSecondAnnouncementIndex(prevIndex => (prevIndex + 2) % announcements.length);
+      }
     }
   }
 
   useEffect(() => {
-    // Clean up function to clear the timeout when the component is unmounted
-    return () => {
-      if (timeOutRef.current !== null) {
-        clearTimeout(timeOutRef.current); // 
-      }
-    };
-  }, []);
+    const interval = setInterval(() => {
+      toggleAnimations();
+    }, displayInterval);
+
+    return () => clearInterval(interval);
+  }, [displayInterval]);
 
   return (
-    <div className={styles.AnnouncementTicker}>
+    <div className={styles.AnnouncementCarousel}>
+
+      {/* First Announcement */}
       <div 
-        className={`${styles.text} ${styles[animationType]}`}
-        onAnimationEnd={handleAnimationEnd}
+        className={`
+          ${styles.announcement}
+          ${styles.first}
+          ${styles[firstAnnouncementAnimation]}
+        `}
+        onAnimationEnd={(event) => handleAnimationEnd(event, 'first')}
       >
-        {announcements[index]}
+        {announcements[firstAnnouncementIndex]}
       </div>
+
+      {/* Second Announcement */}
+      <div 
+        className={`
+          ${styles.announcement} 
+          ${styles.second} 
+          ${secondAnnouncementAnimation ? styles[secondAnnouncementAnimation] : ''}
+        `}
+        onAnimationEnd={(event) => handleAnimationEnd(event, 'second')}
+      >
+        {announcements[secondAnnouncementIndex]}
+      </div>
+
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,14 @@
 import { createRoot } from 'react-dom/client';
-import BannerEnhance from './components/BannerEnhance/BannerEnhance';
+import Banner from './components/Banner/Banner';
 import './main.css';
 
+const announcements = [
+  'Get 10% Off When Buying 2+ Plants',
+  'Free Personalized Gift Notes',
+  'Free Shipping On All Orders',
+  '90-Day Plant Guarantee',
+];
+
 createRoot(document.getElementById('root')!).render(
-  <BannerEnhance />
+  <Banner announcements={announcements} />
 );
